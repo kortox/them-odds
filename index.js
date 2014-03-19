00aeb3534bddc9a78724647fd7a02635995c69d7
Kurt <kortox@gmail.com>
2014-03-18 23:09:13 -0700

    Process level cache

--- index.js
+++ index.js
@@ -21,39 +21,56 @@ app.configure(function(){
 /*
  * Retrieve odds
  */
+var cache = {
+    "expiration" : 0,
+    "items" : []
+};
+
+function renderOddsForSearchTerms(team, feed, response) {
+    var odds = "";
+    for (var i = 0; i < feed.length; i++) {
+        var item = feed[i];
+        var description = item["description"][0];
+        var title = item["title"][0];
+        var re = new RegExp(team.trim(),"gi");
+        var match = title.match(re);
+        if (match) {
+            odds += "<br>" + title + "<br>" + description
+        }
+    }
+    response.send("Odds for " + search_terms + odds);
+}
+
 function getOddsForTeam(content, team, response) {
-    var options = {
-        host: 'www.sportsbooks.com',
-        path: '/lines/cgi/lines.cgi?&sport=201&ct=text/xml'
-    };
-    var req = http.get(options, function(res) {
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
-        // Buffer the body entirely for processing as a whole.
-        var bodyChunks = [];
-        res.on('data', function(chunk) {
-            // collecting chunks
-            bodyChunks.push(chunk);
-        }).on('end', function() { // process the entire body
-            var body = Buffer.concat(bodyChunks);
-            parseString(body, function (err, result) { // we have a parsed up xml object
-                var d = result;
-                var feed = d["rss"]["channel"][0]["item"];
-                var odds = "";
-                for (var i = 0; i < feed.length; i++) {
-                    var item = feed[i];
-                    var description = item["description"][0];
-                    var title = item["title"][0];
-                    var re = new RegExp(team.trim(),"gi");
-                    var match = title.match(re);
-                    if (match) {
-                        odds += "<br>" + title + "<br>" + description
-                    }
-                }
-                response.send("Odds for " + search_terms + odds);
-            });
-        })
-    });
+    if (Date.now() > cache["expiration"]) {
+        var options = {
+            host: 'www.sportsbooks.com',
+            path: '/lines/cgi/lines.cgi?&sport=201&ct=text/xml'
+        };
+        var req = http.get(options, function(res) {
+            console.log('STATUS: ' + res.statusCode);
+            console.log('HEADERS: ' + JSON.stringify(res.headers));
+            // Buffer the body entirely for processing as a whole.
+            var bodyChunks = [];
+            res.on('data', function(chunk) {
+                // collecting chunks
+                bodyChunks.push(chunk);
+            }).on('end', function() { // process the entire body
+                var body = Buffer.concat(bodyChunks);
+                parseString(body, function (err, result) { // we have a parsed up xml object
+                    var d = result;
+                    var feed = d["rss"]["channel"][0]["item"];
+                    //console.log("setting cached data");
+                    cache["expiration"] = Date.now() + 3600000; //1 hour
+                    cache["items"] = feed
+                    renderOddsForSearchTerms(team, feed, response);
+                });
+            })
+        });
+    } else {
+        //console.log("using cached data");
+        renderOddsForSearchTerms(team, cache["items"], response);
+    }
 }
 
 /*
